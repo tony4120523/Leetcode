1a0ae82da9c55de8990e9e23e53a4242b3d94ca3
Hsu Tun <tony4120523@gmail.com>
2019-01-30 19:50:24 +0800

    update comment of the file BestTimeBuyandSellStock

--- BestTimeBuyandSellStock.cpp
+++ BestTimeBuyandSellStock.cpp
@@ -15,9 +15,20 @@ public:
 };
 
 int main() {
+    // to initial the vector, can do ...
+    //
+    // int vv[2] = { 12,43  };
+    // std::vector<int> v(&vv[0], &vv[0]+2);
+    //
+    // or 
+    //
+    // int vv[] = { 12,43  };
+    // std::vector<int> v(begin(vv), end(vv));
+    
     const int a[] = {7, 1, 5, 3, 6, 4};
     vector<int> v(a, a + sizeof(a) / sizeof(a[0]));
 
+    // this solution want to show the answer 5
     Solution s;
     cout << s.maxProfit(v) << "\n";
     return 0;
