06ea97705973e40922779cb3c2c689085aef8c01
dh.hsu <tony4120523@gmail.com>
2019-10-05 16:37:59 +0800

    update ExcelSheetColumnNumber

--- ExcelSheetColumnNumber.cpp
+++ ExcelSheetColumnNumber.cpp
@@ -1,3 +1,24 @@
+// Source : https://oj.leetcode.com/problems/excel-sheet-column-number/
+
+/* Date   : 2019-10-05  r
+ */
+
+/********************************************************************************** 
+ * 
+ * Related to question Excel Sheet Column Title
+ * Given a column title as appear in an Excel sheet, return its corresponding column number.
+ * 
+ * For example:
+ *     A -> 1
+ *     B -> 2
+ *     C -> 3
+ *     ...
+ *     Z -> 26
+ *     AA -> 27
+ *     AB -> 28 
+ *               
+ **********************************************************************************/
+
 #include <iostream>
 using namespace std;
 
