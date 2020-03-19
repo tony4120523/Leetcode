8f7a808e5ce64ef699b107a01ffaf5863549d51e
dh.hsu <tony4120523@gmail.com>
2020-03-19 11:46:34 +0800

    update MaximumDepthofBinaryTree

--- MaximumDepthofBinaryTree.cpp
+++ MaximumDepthofBinaryTree.cpp
@@ -1,3 +1,17 @@
+// Source : https://oj.leetcode.com/problems/maximum-depth-of-binary-tree/
+
+/* Date   : 2020-03-19  r
+ */
+
+/********************************************************************************** 
+* 
+* Given a binary tree, find its maximum depth.
+* 
+* The maximum depth is the number of nodes along the longest path from the root node 
+* down to the farthest leaf node.
+*               
+**********************************************************************************/
+
 #include <iostream>
 #include <vector>
 #include <string>
