48c0a6bd3add5c5c65aa831aa734f52cd8efc8a6
dh.hsu <tony4120523@gmail.com>
2020-03-30 16:59:16 +0800

    update SumOfTwoIntegers

--- SumOfTwoIntegers.cpp
+++ SumOfTwoIntegers.cpp
@@ -3,6 +3,7 @@
 /* Date   : 2019-10-01  r
  *          2019-10-15  r
  *          2019-11-30  r
+ *          2020-03-30  r
  */
 
 /*************************************************************************************** 
@@ -13,7 +14,6 @@
  * Example:
  * Given a = 1 and b = 2, return 3.
  * 
- * 
  ***************************************************************************************/
 
 #include <iostream>
@@ -23,10 +23,10 @@ class Solution {
 public:
     int getSum(int a, int b) {
         while (b != 0) {
-            /**
-             * 1 : find carry position, 
-             * 2 : zero the 1,1 position, remain 1,0 and 0,1
-             * 3 : shift carry left 1 position
+            /*
+             * 1. find carry position, 
+             * 2. zero the 1,1 position, remain 1,0 and 0,1
+             * 3. shift carry left 1 position
              */
             int car = a & b;
             a = a ^ b;
@@ -34,12 +34,17 @@ public:
         }
         return a;        
     }
+    /*
+     *  #1 round.  car = 11 & 10 = 10 , a = 11 ^ 10 = 01 , b = 10 << 1 = 100
+     *  #2 round.  car = 001 & 100 = 000 , a = 001 ^ 100 = 101 , b = 000 << 1 = 0000
+     *             a = 101 -> return !!!
+     */
 };
 
 int main() {
     int a = 3, b = 2;
     Solution s;
-    cout << s.getSum(a, b) << "\n";
+    cout << s.getSum(a, b) << "\n"; // result: 5
     return 0;
 }
 
