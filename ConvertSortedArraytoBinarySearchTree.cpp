d53b29c59cc1c29f0b15f3a5d8b63784363e90ef
dh.hsu <tony4120523@gmail.com>
2020-06-02 15:57:28 +0800

    update Convert Sorted Array To Binary Search Tree

--- ConvertSortedArraytoBinarySearchTree.cpp
+++ ConvertSortedArraytoBinarySearchTree.cpp
@@ -1,4 +1,8 @@
-/**
+// Source : https://oj.leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
+/* Date   : 2020-06-02  r
+ */
+
+/*****
  * Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
 
  * For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
@@ -12,7 +16,8 @@
    -3   9
    /   /
  -10  5
- */
+
+ *****/
 
 #include <iostream>
 #include <vector>
@@ -57,7 +62,7 @@ int main() {
     vector<int> T(arr, arr + 5);
 
     Solution s;
-    s.print(s.sortedArrayToBST(T));
+    s.print(s.sortedArrayToBST(T)); // result: {-10, -3, 0, 5, 9}
     return 0;
 }
 
